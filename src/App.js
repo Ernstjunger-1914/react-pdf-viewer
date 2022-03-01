120c731646479b19d1e3aec700253030dcefcb51
손소독 <vot768@naver.com>
2022-03-01 17:20:00 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import './App.css';
 
 function App() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({numPages}) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <div>
+          <Document file="/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+            {
+              Array.from(
+              new Array(numPages),
+              (el,index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber = {index + 1}
+                />
+              )
+            )}
+          </Document>
+        </div>
+      </center>
     </div>
   );
 }
